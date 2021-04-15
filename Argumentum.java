cddb475cee85c852a20ef47f6d15b52cf6040d00
Sárdi András <sardi.andras@gmail.com>
2021-04-15 22:45:05 +0200

    Create Argumentum.java

---
+++ Argumentum.java
@@ -0,0 +1,9 @@
+public class Argumentum {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<args.length) {
+            System.out.println(i + ". argumentum: " + args[i]);
+            i++;
+        }
+    }
+}
